2c399524946b34c44313580318c3c0e6c6790876
unknown <marcus.vini2@hotmail.com>
2018-08-20 11:07:29 -0300

    Starts screenshoter and patter regocnition modules

---
+++ screenshot.py
@@ -0,0 +1,13 @@
+#-- include('examples/showgrabfullscreen.py') --#
+import pyscreenshot as ImageGrab
+
+if __name__ == '__main__':
+
+    # grab fullscreen
+    im = ImageGrab.grab()
+
+    # save image file
+    im.save('screenshot.png')
+
+
+#-#
