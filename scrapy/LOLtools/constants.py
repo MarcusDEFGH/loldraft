e77d83f0bad187239a5268089d20ba0da439efbd
unknown <marcus.vini2@hotmail.com>
2018-10-02 20:13:28 -0300

    Renames folders

---
+++ scrapy/LOLtools/constants.py
@@ -0,0 +1,19 @@
+XPATHS_GAME = {
+    'mmr': '//span[@class="tierRank"]/text()',
+    'result': './/div[@class="GameResult"]/text()',
+    '_match_type': './/div[@class="GameType"]/text()',
+    '_matches': '//div[@class="GameItemWrap"]',
+    'champions_team_1': './/div[@class="Team"][1]//div[@class="ChampionImage"]',
+    'champions_team_2': './/div[@class="Team"][2]//div[@class="ChampionImage"]',
+    'summoners_team_1': './/div[@class="Team"][1]//div[@class="SummonerName"]',
+    'summoners_team_2': './/div[@class="Team"][2]//div[@class="SummonerName"]',
+    'champion_name': './/div/text()',
+    'timestamp': './/div[@class="TimeStamp"]//span/text()',
+    'profile_link': './/a/@href',
+    'name': '//div[@class="Information"]/span//text()'
+}
+
+
+XPATHS_LADDER = {
+    '_summoners': '//a[not(contains(@class, "ranking-highest__name"))]//@href[contains(.,"user")]'
+}
