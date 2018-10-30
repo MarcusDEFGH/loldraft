faed711a58c136e63c1a4600176ef19d5cf5a40b
Marcus Teixeira <marcusvini211@gmail.com>
2018-10-30 14:32:30 -0300

    Delete celery.py

--- knn/celery.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from celery import Celery
-
-app = Celery('knn-celery', broker='amqp://guest:guest@rabbitmq:5672/guest',
-             backend='rpc://', include=['knn.tasks'])
